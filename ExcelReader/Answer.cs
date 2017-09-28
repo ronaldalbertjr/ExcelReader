f1d3b0ac34e0cd01f6f62bb64a389ca18783ba0f
ronaldalbertjr <ronaldalbert1609@gmail.com>
2017-09-28 07:50:24 -0300

    BigPartMade

--- ExcelReader/Answer.cs
+++ ExcelReader/Answer.cs
@@ -8,5 +8,50 @@ namespace ExcelReader
 {
     class Answer
     {
+        public string time;
+        public string name;
+        public string group;
+        public string[] answers = new string[24];
+
+        public Answer[] CheckMatch(Answer[] answers)
+        {
+            float[] points = new float[answers.Length];
+            for(int i = 0; i < answers.Length; i++)
+            {
+                if((answers[i].answers[0] == "Sim" && this.answers[0] == "Não") || (answers[i].answers[0] == "Não" && this.answers[0] == "Sim"))
+                {
+                    points[i] += 0.5f;
+                }
+                if( (answers[i].answers[1] == "Inverno" && this.answers[1] == "Inverno") || (answers[i].answers[1] == "Verão" && this.answers[1] == "Verão") || (answers[i].answers[1] == "Primavera" && this.answers[1] == "Outono") || (answers[i].answers[1] == "Outono" && this.answers[1] == "Primavera"))
+                {
+                    points[i] += 1;
+                }
+                else if((answers[i].answers[1] == "Primavera" && this.answers[1] == "Verão") || (answers[i].answers[1] == "Verão" && this.answers[1] == "Primavera") || (answers[i].answers[1] == "Outono" && this.answers[1] == "Inverno") || (answers[i].answers[1] == "Inverno" && this.answers[1] == "Outono"))
+                {
+                    points[i] += 0.5f;
+                }
+                if((answers[i].answers[2] == "Habbib's" && this.answers[2] == "Subway") || (answers[i].answers[2] == "Subway" && this.answers[2] == "Habbib's") || (answers[i].answers[2] == "Burger King" && this.answers[2] == "Burger King") || (answers[i].answers[2] == "McDonald's" && this.answers[2] == "Bob's") || (answers[i].answers[2] == "Bob's" && this.answers[2] == "McDonald's") || (answers[i].answers[2] == "KFC" && this.answers[2] == "Burguer King") || (answers[i].answers[2] == "Burguer King" && this.answers[2] == "KFC"))
+                {
+                    points[i] += 1;
+                }
+                else if((answers[i].answers[2] == "McDonald's" && this.answers[2] == "Burguer King") || (answers[i].answers[2] == "Burguer King" && this.answers[2] == "McDonald's"))
+                {
+                    points[i] += 0.5f;
+                }
+                if((answers[i].answers[3] == "Metro Linha 1" && this.answers[3] == "VLT") || (answers[i].answers[3] == "VLT" && this.answers[3] == "Metro Linha 1") || (answers[i].answers[3] == "BRT" && this.answers[3] == "Metro Linha 2") || (answers[i].answers[3] == "Metro Linha 2" && this.answers[3] == "BRT") || (answers[i].answers[3] == "VLT" && this.answers[3] == "VLT"))
+                {
+                    points[i] += 1;
+                }
+                else if((answers[i].answers[3] == "Metro Linha 1" && this.answers[3] == "Metro Linha 2") || (answers[i].answers[3] == "Metro Linha 2" && this.answers[3] == "Metro Linha 1"))
+                {
+                    points[i] += 0.5f;
+                }
+                if((answers[i].answers[4] == "Sim" && answers[i].answers[4] == "Não") || (answers[i].answers[4] == "Não" && answers[i].answers[4] == "Sim") || (answers[i].answers[4] == "Irrelevante" && answers[i].answers[4] == "Que?") || (answers[i].answers[4] == "Que?" && answers[i].answers[4] == "Irrelevante"))
+                {
+                    points[i] += 1f;
+                }
+            }
+            return new Answer[10];
+        }
     }
 }
--- ExcelReader/Program.cs
+++ ExcelReader/Program.cs
@@ -10,27 +10,37 @@ namespace ExcelReader
 {
     class Program
     {
+        static string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocume‌​nts) + "\\oi.xlsx";
+        static Answer[] answers;
         static void Main(string[] args)
         {
             Excel.Application xlApp = new Excel.Application();
-            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"oi.xlsx");
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(path);
             Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
             Excel.Range xlRange = xlWorksheet.UsedRange;
 
             int rowCount = xlRange.Rows.Count;
             int columnCount = xlRange.Columns.Count;
-
-            for(int i = 1; i <= rowCount; i++)
+            answers = new Answer[rowCount];
+            for(int i = 2; i <= rowCount; i++)
             {
                 for(int j = 1; j <= columnCount; j++)
                 {
                     if(j == 1)
                     {
-                        Console.Write("\r\n");
+                        answers[i - 2] = new Answer();
                     }
+
                     if(xlRange.Cells[i,j] != null && xlRange.Cells[i,j].Value2 != null)
                     {
-                        Console.Write(xlRange.Cells[i,j].Value2.ToString() + "\t");
+                        if(j == 1)
+                            answers[i - 2].time = xlRange.Cells[i, j].Value2.ToString();
+                        else if(j == 2)
+                            answers[i - 2].name = xlRange.Cells[i, j].Value2.ToString();
+                        else if(j == 3)
+                            answers[i - 2].group = xlRange.Cells[i, j].Value2.ToString();
+                        else
+                            answers[i - 2].answers[j-4] = xlRange.Cells[i, j].Value2.ToString();
                     }
                 }
             }
@@ -46,6 +56,8 @@ namespace ExcelReader
 
             xlApp.Quit();
             Marshal.ReleaseComObject(xlApp);
+
+            Console.ReadKey();
         }
     }
 }
