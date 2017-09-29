81b996c835272dab441942c67dce5ece8d2c80f4
Ronald Albert <ronaldalbert1609@gmail.com>
2017-09-29 10:49:13 -0300

    AddedSomeAnswers

--- ExcelReader/Answer.cs
+++ ExcelReader/Answer.cs
@@ -46,7 +46,39 @@ namespace ExcelReader
                 {
                     points[i] += 0.5f;
                 }
-                if((answers[i].answers[4] == "Sim" && answers[i].answers[4] == "Não") || (answers[i].answers[4] == "Não" && answers[i].answers[4] == "Sim") || (answers[i].answers[4] == "Irrelevante" && answers[i].answers[4] == "Que?") || (answers[i].answers[4] == "Que?" && answers[i].answers[4] == "Irrelevante"))
+                if((answers[i].answers[4] == "Sim" && this.answers[4] == "Não") || (answers[i].answers[4] == "Não" && this.answers[4] == "Sim") || (answers[i].answers[4] == "Irrelevante" && this.answers[4] == "Que?") || (answers[i].answers[4] == "Que?" && this.answers[4] == "Irrelevante"))
+                {
+                    points[i] += 1f;
+                }
+                if ((answers[i].answers[5] == "Tempestade" && this.answers[5] == "Wolverine") || (answers[i].answers[5] == "Wolverine" && this.answers[5] == "Tempestade") || (answers[i].answers[4] == "Fênix (Jean Grey)" && this.answers[4] == "Ciclope (Scott Summers)") || (answers[i].answers[4] == "Ciclope (Scott Summers)" && this.answers[4] == "Fênix (Jean Grey)") || (answers[i].answers[4] == "Magneto" && this.answers[4] == "Professor Xavier") || (answers[i].answers[4] == "Professor Xavier" && this.answers[4] == "Magneto") || (answers[i].answers[4] == "Noturno" && this.answers[4] == "Vampira") || (answers[i].answers[4] == "Vampira" && this.answers[4] == "Noturno"))
+                {
+                    points[i] += 1f;
+                }
+                else if((answers[i].answers[5] == "Ciclope (Scott Summers)" && this.answers[5] == "Wolverine") || (answers[i].answers[5] == "Wolverine" && this.answers[5] == "Ciclope (Scott Summers)") || (answers[i].answers[5] == "Noturno" && this.answers[5] == "Lince Negra") || (answers[i].answers[5] == "Lince Negra" && this.answers[5] == "Noturno") || (answers[i].answers[5] == "Magneto" && this.answers[5] == "Mística") || (answers[i].answers[5] == "Mística" && this.answers[5] == "Magneto"))
+                {
+                    points[i] += 0.75f;
+                }
+                else if((answers[i].answers[5] == "Wolverine" && this.answers[5] == "Fênix (Jean Grey)") || (answers[i].answers[5] == "Fênix (Jean Grey)" && this.answers[5] == "Wolverine") || (answers[i].answers[5] == "Mística" && this.answers[5] == "Wolverine") || (answers[i].answers[5] == "Wolverine" && this.answers[5] == "Mística") || (answers[i].answers[5] == "Professor Xavier" && this.answers[5] == "Mística") || (answers[i].answers[5] == "Mística" && this.answers[5] == "Professor Xavier"))
+                {
+                    points[i] += 0.5f;
+                }
+                if((answers[i].answers[6] == "Juliana Potengy" && this.answers[6] == "Juliana Potengy") || (answers[i].answers[6] == "Thais Dutra" && this.answers[6] == "Thais Dutra") || (answers[i].answers[6] == "Andressa Alves" && this.answers[6] == "Ana Julia Borges") || (answers[i].answers[6] == "Ana Julia Borges" && this.answers[6] == "Andressa Alves"))
+                {
+                    points[i] += 1f;
+                }
+                else if((answers[i].answers[6] == "Juliana Potengy" && this.answers[6] == "Evelyn Correa") || (answers[i].answers[6] == "Evelyn Correa" && this.answers[6] == "Juliana Potengy") || (answers[i].answers[6] == "Juliana Potengy" && this.answers[6] == "Eduarda Helena") || (answers[i].answers[6] == "Eduarda Helena" && this.answers[6] == "Juliana Potengy"))
+                {
+                    points[i] += 0.5f;
+                }
+                if(answers[i].answers[7] == this.answers[7])
+                {
+                    points[i] += 1f;
+                }
+                if(answers[i].answers[8] == this.answers[8])
+                {
+                    points[i] += 1f;
+                }
+                if((answers[i].answers[9] == "Sorriso" && this.answers[9] == "Olhos") || (answers[i].answers[9] == "Olhos" && this.answers[9] == "Sorriso") || (answers[i].answers[9] == "Cabelo" && this.answers[9] == "Cabelo"))
                 {
                     points[i] += 1f;
                 }
